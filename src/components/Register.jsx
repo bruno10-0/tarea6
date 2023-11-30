23cae0e5920d1ac03682d4f065b234f70a3b4148
bruno10-0 <brunobrodon75@gmail.com>
2023-11-30 20:25:32 -0300

    validations

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import {
   Box,
   Button,
   ChakraProvider,
   extendTheme,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   Stack,
   Heading,
 } from '@chakra-ui/react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const theme = extendTheme({
   styles: {
@@ -22,6 +26,31 @@ const theme = extendTheme({
 });
 
 const Register = () => {
+  const formik = useFormik({
+    initialValues: {
+      nombre: '',
+      apellido: '',
+      email: '',
+      telefono: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required('Nombre es requerido'),
+      apellido: Yup.string().required('Apellido es requerido'),
+      email: Yup.string().email('Email no válido').required('Email es requerido'),
+      telefono: Yup.string().required('Teléfono es requerido'),
+      password: Yup.string().required('Password es requerido'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Las contraseñas deben coincidir')
+        .required('Confirmar Password es requerido'),
+    }),
+    onSubmit: (values) => {
+      // Aquí puedes manejar la lógica para enviar los datos al servidor
+      console.log('Datos registrados:', values);
+    },
+  });
+
   return (
     <ChakraProvider theme={theme}>
       <Box
@@ -33,42 +62,76 @@ const Register = () => {
         boxShadow="lg"
         textAlign="center"
       >
-        <Heading mb="4" color="teal.300">Registrarse</Heading>
-        <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="nombre">Nombre</FormLabel>
-            <Input type="text" id="nombre" />
-          </FormControl>
+        <Heading mb="4" color="teal.300">
+          Registrarse
+        </Heading>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={4}>
+            <FormControl isInvalid={formik.touched.nombre && formik.errors.nombre}>
+              <FormLabel htmlFor="nombre">Nombre</FormLabel>
+              <Input
+                type="text"
+                id="nombre"
+                {...formik.getFieldProps('nombre')}
+              />
+              <FormErrorMessage>{formik.errors.nombre}</FormErrorMessage>
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="apellido">Apellido</FormLabel>
-            <Input type="text" id="apellido" />
-          </FormControl>
+            <FormControl isInvalid={formik.touched.apellido && formik.errors.apellido}>
+              <FormLabel htmlFor="apellido">Apellido</FormLabel>
+              <Input
+                type="text"
+                id="apellido"
+                {...formik.getFieldProps('apellido')}
+              />
+              <FormErrorMessage>{formik.errors.apellido}</FormErrorMessage>
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input type="email" id="email" />
-          </FormControl>
+            <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                type="email"
+                id="email"
+                {...formik.getFieldProps('email')}
+              />
+              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="telefono">Teléfono</FormLabel>
-            <Input type="tel" id="telefono" />
-          </FormControl>
+            <FormControl isInvalid={formik.touched.telefono && formik.errors.telefono}>
+              <FormLabel htmlFor="telefono">Teléfono</FormLabel>
+              <Input
+                type="tel"
+                id="telefono"
+                {...formik.getFieldProps('telefono')}
+              />
+              <FormErrorMessage>{formik.errors.telefono}</FormErrorMessage>
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input type="password" id="password" />
-          </FormControl>
+            <FormControl isInvalid={formik.touched.password && formik.errors.password}>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                type="password"
+                id="password"
+                {...formik.getFieldProps('password')}
+              />
+              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="confirmPassword">Confirmar Password</FormLabel>
-            <Input type="password" id="confirmPassword" />
-          </FormControl>
+            <FormControl isInvalid={formik.touched.confirmPassword && formik.errors.confirmPassword}>
+              <FormLabel htmlFor="confirmPassword">Confirmar Password</FormLabel>
+              <Input
+                type="password"
+                id="confirmPassword"
+                {...formik.getFieldProps('confirmPassword')}
+              />
+              <FormErrorMessage>{formik.errors.confirmPassword}</FormErrorMessage>
+            </FormControl>
 
-          <Button colorScheme="teal" type="submit">
-            Registrar
-          </Button>
-        </Stack>
+            <Button colorScheme="teal" type="submit">
+              Registrar
+            </Button>
+          </Stack>
+        </form>
       </Box>
     </ChakraProvider>
   );
